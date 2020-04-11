423de73b8d136c2d8096882a582beda6cb824a26
Stacks-Mamba <stexor12@gmail.com>
2020-04-11 14:58:01 +0100

    Add scan methods for tokens

--- src/lexer.py
+++ src/lexer.py
@@ -16,36 +16,165 @@ class Lexer:
         if self.current_char == "":
             self.current_char = Lexer.EOF
 
+    def Lookahead(self):
+        last_position = self.file.tell()
+        next_char = self.file.read(1)
+        self.file.seek(last_position)
+        return next_char
+
+    def error(self):
+        raise Exception("Error during lexing phase")
+
+
+
     def whitespace(self):
         while self.current_char.isspace() and self.current_char is not None:
+            if self.current_char == "\n":
+                self.line += 1
             self.advance()
 
-    def integer(self):
+    def arit_operators(self):
+        if self.current_char == "+":
+            self.advance()
+            return Token(TokenType.PLUS,"+")
+
+        elif self.current_char == "-":
+            self.advance()
+            return Token(TokenType.MINUS,"-")
+
+        elif self.current_char == "*":
+            self.advance()
+            return Token(TokenType.STAR,"*")
+
+        elif self.current_char == "/":
+            self.advance()
+            return Token(TokenType.SLASH,"/")
+
+        elif self.current_char == "%":
+            self.advance()
+            return Token(TokenType.MODULO,"%")
+
+    def logic_operators(self):
+        if self.current_char == "<":
+            if self.Lookahead() == "=":
+                self.advance()
+                self.advance()
+                return Token(TokenType.LESSEQ,"<=")
+            self.advance()
+            return Token(TokenType.LESS,"<")
+
+        elif self.current_char == ">":
+            if self.Lookahead() == "=":
+                self.advance()
+                self.advance()
+                return Token(TokenType.GREATEREQ,">=")
+            self.advance()
+            return Token(TokenType.GREATER,">")
+
+        elif self.current_char == "=":
+            if self.Lookahead() == "=":
+                self.advance()
+                self.advance()
+                return Token(TokenType.DOUBLEEQUAL,"==")
+            self.advance()
+            return Token(TokenType.EQUAL,"=")
+
+        elif self.current_char == "!":
+            if self.Lookahead() == "=":
+                self.advance()
+                self.advance()
+                return Token(TokenType.NOTEQUAL,"!=")
+            self.error()
+
+
+    def number(self):
         result = ""
         while self.current_char.isdigit():
             result += self.current_char
             self.advance()
-        return int(result)
+        if self.current_char == ".":
+            #Lookahead to see next char i
+            if self.Lookahead().isdigit():
+                result += "."
+                self.advance()
+                while self.current_char.isdigit():
+                    result += self.current_char
+                    self.advance()
+        if "." in result:
+            return Token(TokenType.DECIMAL,float(result))
+        return Token(TokenType.INTEGER,int(result))
+
+
+
+    def string(self):
+        result = ""
+        symbol = self.current_char
+        self.advance()
+        while self.current_char != symbol and self.current_char  != Lexer.EOF:
+            result += self.current_char
+            self.advance()
+        if self.current_char == Lexer.EOF:
+            self.error()
+        return Token(TokenType.STRING,result)
+
+
+    def identifier(self):
+        result = ""
+        while self.current_char.isalnum() or self.current_char == "_":
+            result += self.current_char
+            self.advance()
+        return Token(TokenType.IDENTIFIER,result)
+
+    def delimeters(self):
+        if self.current_char == ")":
+            self.advance()
+            return Token(TokenType.RPAR,self.current_char)
+        elif self.current_char == "(":
+            self.advance()
+            return Token(TokenType.LPAR,self.current_char)
+        elif self.current_char == ".":
+            self.advance()
+            return Token(TokenType.DOT,self.current_char)
+        elif self.current_char == ";":
+            self.advance()
+            return Token(TokenType.SEMI,self.current_char)
+        elif self.current_char == ",":
+            self.advance()
+            return Token(TokenType.COMMA,self.current_char)
+
+
 
     def get_next_token(self):
         if self.current_char.isspace():
             self.whitespace()
 
+        #arit_operators
+        if self.current_char in ["+","-","*","/","%"]:
+            return self.arit_operators()
+
+        #logic ops
+        if self.current_char in ["<",">","!","="]:
+            return self.logic_operators()
+
+        #numbers (real and integer)
         if self.current_char.isdigit():
-            return Token(TokenType.INTEGER,self.integer())
+            return self.number()
 
-        if self.current_char == "+":
-            self.advance()
-            return Token(TokenType.PLUS,"+")
+        #Strings
+        if self.current_char == "'" or self.current_char == '"':
+            return self.string()
 
-        if self.current_char == "-":
-            self.advance()
-            return Token(TokenType.MINUS,"-")
+        #Ids
+        if self.current_char.isalpha() or self.current_char == "_":
+            return self.identifier()
+
+        #delims
+        if self.current_char in ["(",")",".",";",","]:
+            return self.delimeters()
 
         if self.current_char == Lexer.EOF:
             return Token(Lexer.EOF,"")
-
-        raise TypeError
+        self.error()
 
 
 
