95417cd0b596b8d98b30c6a87d99034df128389c
Stacks-Mamba <stexor12@gmail.com>
2020-05-05 17:30:25 +0100

    Add remaining tests to test runner
    This change adds code that runs the remainder of the tests in the
    test suite

--- test_runner.py
+++ test_runner.py
@@ -68,13 +68,160 @@ class TestPTI(unittest.TestCase):
         res_file.close()
 
 
-    @unittest.expectedFailure
-    def test_assignment(self):
-        for test in assign_tests:
-            with open(test[0],"r") as file:
-                run_script(file,self.buffer)
-            self.assertEqual(self.buffer.getvalue(),test[1],f"Assignment test failure. file: {test[0]}")
-            self.buffer = StringIO()
+    def test_assign(self):
+        results = join(ASSIGNMENT,"result.txt")
+        res_file = open(results,"r")
+        for root,dirs,files in os.walk(ASSIGNMENT):
+            for file in sorted(files):
+                if join(ASSIGNMENT,file) == results:
+                    continue
+                with open(join(root,file),"r") as script:
+                    run_script(script,self.buffer)
+                self.assertEqual(self.buffer.getvalue()
+                ,res_file.readline().strip(),
+                f"Assignment test failure. file: {file}")
+                self.buffer = StringIO()
+                #assert result
+        res_file.close()
+
+
+    def test_declaration(self):
+        results = join(DECLARATION,"result.txt")
+        res_file = open(results,"r")
+        for root,dirs,files in os.walk(DECLARATION):
+            for file in sorted(files):
+                if join(DECLARATION,file) == results:
+                    continue
+                with open(join(root,file),"r") as script:
+                    run_script(script,self.buffer)
+                self.assertEqual(self.buffer.getvalue()
+                ,res_file.readline().strip(),
+                f"declaration test failure. file: {file}")
+                self.buffer = StringIO()
+                #assert result
+        res_file.close()
+
+
+
+    def test_expression(self):
+        results = join(EXPRESSION,"result.txt")
+        res_file = open(results,"r")
+        for root,dirs,files in os.walk(EXPRESSION):
+            for file in sorted(files):
+                if join(EXPRESSION,file) == results:
+                    continue
+                with open(join(root,file),"r") as script:
+                    run_script(script,self.buffer)
+                self.assertEqual(self.buffer.getvalue()
+                ,res_file.readline().strip(),
+                f"expression test failure. file: {file}")
+                self.buffer = StringIO()
+                #assert result
+        res_file.close()
+
+    def test_function(self):
+        results = join(FUNCTION,"result.txt")
+        res_file = open(results,"r")
+        for root,dirs,files in os.walk(FUNCTION):
+            for file in sorted(files):
+                if join(FUNCTION,file) == results:
+                    continue
+                with open(join(root,file),"r") as script:
+                    run_script(script,self.buffer)
+                self.assertEqual(self.buffer.getvalue()
+                ,res_file.readline().strip(),
+                f"function test failure. file: {file}")
+                self.buffer = StringIO()
+                #assert result
+        res_file.close()
+
+
+    def test_enquanto(self):
+        results = join(ENQUANTO,"result.txt")
+        res_file = open(results,"r")
+        for root,dirs,files in os.walk(ENQUANTO):
+            for file in sorted(files):
+                if join(ENQUANTO,file) == results:
+                    continue
+                with open(join(root,file),"r") as script:
+                    run_script(script,self.buffer)
+                self.assertEqual(self.buffer.getvalue()
+                ,res_file.readline().strip(),
+                f"Enquanto test failure. file: {file}")
+                self.buffer = StringIO()
+                #assert result
+        res_file.close()
+
+
+
+    def test_mostra(self):
+        results = join(MOSTRA,"result.txt")
+        res_file = open(results,"r")
+        for root,dirs,files in os.walk(MOSTRA):
+            for file in sorted(files):
+                if join(MOSTRA,file) == results:
+                    continue
+                with open(join(root,file),"r") as script:
+                    run_script(script,self.buffer)
+                self.assertEqual(self.buffer.getvalue()
+                ,res_file.readline().strip(),
+                f"Mostra test failure. file: {file}")
+                self.buffer = StringIO()
+                #assert result
+        res_file.close()
+
+
+    def test_para(self):
+        results = join(PARA,"result.txt")
+        res_file = open(results,"r")
+        for root,dirs,files in os.walk(PARA):
+            for file in sorted(files):
+                if join(PARA,file) == results:
+                    continue
+                with open(join(root,file),"r") as script:
+                    run_script(script,self.buffer)
+                self.assertEqual(self.buffer.getvalue()
+                ,res_file.readline().strip(),
+                f"Para test failure. file: {file}")
+                self.buffer = StringIO()
+                #assert result
+        res_file.close()
+
+
+    def test_retorna(self):
+        results = join(RETORNA,"result.txt")
+        res_file = open(results,"r")
+        for root,dirs,files in os.walk(RETORNA):
+            for file in sorted(files):
+                if join(RETORNA,file) == results:
+                    continue
+                with open(join(root,file),"r") as script:
+                    run_script(script,self.buffer)
+                self.assertEqual(self.buffer.getvalue()
+                ,res_file.readline().strip(),
+                f"Retorna test failure. file: {file}")
+                self.buffer = StringIO()
+                #assert result
+        res_file.close()
+
+    def test_se(self):
+        results = join(SE,"result.txt")
+        res_file = open(results,"r")
+        for root,dirs,files in os.walk(SE):
+            for file in sorted(files):
+                if join(SE,file) == results:
+                    continue
+                with open(join(root,file),"r") as script:
+                    run_script(script,self.buffer)
+                self.assertEqual(self.buffer.getvalue()
+                ,res_file.readline().strip(),
+                f"Se test failure. file: {file}")
+                self.buffer = StringIO()
+                #assert result
+        res_file.close()
+
+
+
 
 
 
