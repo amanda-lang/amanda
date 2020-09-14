d163841211c1e9ece9109b4bcd00a1bed1007a7b
Stacks-Mamba <stexor12@gmail.com>
2020-09-14 18:25:07 +0100

    Finish build function

--- setup.py
+++ setup.py
@@ -4,18 +4,14 @@ import shutil
 import sys
 import PyInstaller.__main__ as pyinstaller_main
 
-
-
 OS_X = sys.platform == "darwin"
 WIN_32 = sys.platform == "win32"
 LINUX = sys.platform == "linux"
 
-
 def main():
     BINARY_NAME = "amanda"
-    SCRIPT = path.abspath(path.join("./amanda","__main__.py"))
-    BUILD_DIR = path.abspath("./dist")
-
+    SCRIPT = path.join("./amanda","__main__.py")
+    BUILD_DIR = "./dist"
     #Build executable
     pyinstaller_main.run([
         f"--name={BINARY_NAME}",
@@ -28,8 +24,14 @@ def main():
     os.remove(f"{BINARY_NAME}.spec")
     shutil.rmtree("./build")
 
+    #Create a symlink pointing to binary in /usr/local/bin/ 
+    #in Mac and linux
+    if OS_X or LINUX:
+        os.symlink(
+            path.join(BUILD_DIR,BINARY_NAME),
+            path.join(path.abspath("/usr/local/bin"),BINARY_NAME)
+        )
 
 
 if __name__ == "__main__":
-    #print(os.environ["PATH"])
     main()
