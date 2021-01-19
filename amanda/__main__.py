32c621f24d50ef46787d7931beb04dbfc3a3b888
Sténio Jacinto <stexor12@gmail.com>
2021-01-19 20:27:20 +0100

    Add event logging (#65)
    This commit adds basic event logging to amanda.
    The logging feature can be used by external programs to check for certain code
    events

--- amanda/__main__.py
+++ amanda/__main__.py
@@ -1,21 +1,26 @@
 import argparse
 import time
 from io import StringIO
+import os
 import sys
 from os.path import abspath
+from amanda.events import (
+    open_conn, close_conn, create_event_hook
+)
 from amanda.compile import ama_compile
 from amanda.error import handle_exception,throw_error
 from amanda.bltins import bltin_objs
 
-
 def main(*args):
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", help = "source file to be executed")
+    parser.add_argument("file", help="source file to be executed")
     parser.add_argument("-g","--generate", help="Generate an output file", action = "store_true")
     parser.add_argument(
-        "-o","--outname", type = str,
+        "-o","--outname", type=str,
         help = "Name of the output file, Requires the -g option to take effect. Defaults to output.py."
     )
+    parser.add_argument("-r","--report", help="Activates report mode and sends event messages to specified port on the local machine.", type=int)
+
     if len(args):
         args = parser.parse_args(args)
     else:
@@ -36,15 +41,28 @@ def main(*args):
         with open(out_file,"w") as output:
             output.write(code)
 
-    #Run compiled python code
+    #Send events to specified port;
+    if args.report:
+        socket = open_conn(args.report)
+        if socket is None:
+            sys.stderr.write("Unable to connect to specified port.\n")
+            sys.exit()
+        hook = create_event_hook(socket)
+        sys.addaudithook(hook)
+   
     pycode_obj = compile(code,out_file,"exec")
     try:
+        #Run compiled python code
         exec(pycode_obj,bltin_objs)
     except Exception as e:
         ama_error = handle_exception(e,out_file,line_info)
         if not ama_error:
             raise e
         throw_error(ama_error,src)
+    finally:
+        #Close open connection
+        if args.report:
+            close_conn(socket)
 
 if __name__ == "__main__":
     main()
---
+++ amanda/events.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import socket
+
+def open_conn(port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(("127.0.0.1", port))
+        return s
+    except Exception as e:
+        sys.stderr.write(str(e) + "\n")
+        return None
+
+def close_conn(socket):
+    socket.close()
+
+def event_hook(event, args):
+    if event == "builtins.input":
+        return b"<INPUT>"
+    return None
+
+def create_event_hook(socket):
+    def event_wrapper(*args, **kwargs):
+        message = event_hook(*args, **kwargs)
+        if message != None:
+            socket.sendall(message)
+    return event_wrapper
+
+
+
+
+
