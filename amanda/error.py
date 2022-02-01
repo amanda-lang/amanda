f5ac7e0a3ead870dd7f811723553e1983485ab40
stackswithans <stexor12@gmail.com>
2022-02-01 21:25:25 +0100

    Add mypy config

--- amanda/error.py
+++ amanda/error.py
@@ -1,100 +1,72 @@
 import sys
+from os import path
 
 
 class AmandaError(Exception):
-    def __init__(self, message, line, col=0):
+    # error types
+    SYNTAX_ERR = 0
+    OTHER_ERR = 1
+
+    def __init__(self, err_type, fpath, message, line, col=0):
+        self.err_type = err_type
+        self.fpath = fpath
         self.message = message
         self.line = line
         self.col = col
 
     @classmethod
-    def syntax_error(cls, message, line, col):
-        instance = cls(message, line)
-        instance.message = f"\nErro sintático na linha {line}: {message}.\n"
-        return instance
+    def syntax_error(cls, fpath, message, line, col):
+        return cls(cls.SYNTAX_ERR, fpath, message, line, col)
 
     @classmethod
-    def common_error(cls, message, line):
-        instance = cls(message, line)
-        instance.message = f"\nErro na linha {line}: {message}.\n"
-        return instance
+    def common_error(cls, fpath, message, line):
+        return cls(cls.OTHER_ERR, fpath, message, line)
 
     def __str__(self):
         return self.message
 
 
-def get_context(error, source):
-    """
-    Gets the context of an error. the context
-    is just just an array with a certain number of lines
-    from the source file.
-    """
-    context = []
-    source.seek(0)
-    # number of lines to use as context
-    n_lines = 2
-    # range to get
-    lower_bound = error.line - n_lines
-    upper_bound = error.line
-
-    for count, line in enumerate(source):
-        # get lines that are within context range
-        if count + 1 >= lower_bound:
-            fmt_line = "| ".join([str(count + 1), line.strip()])
-            context.append(fmt_line)
-            if count + 1 == upper_bound:
-                source.close()
-                break
-    return context
-
-
-def fmt_error(context, error):
+def fmt_error(context: str, error: AmandaError):
     """
     Formats the error message using the error
     object and the context.
 
     Ex:
 
-    Erro sintático na linha 5: alguma mensagem aqui
-    ------------------------------------------------
-
-    3| var char : texto
-    4| var lobo : Animal
-    5| var cao : Animal func
-                        ^^^^
+    Ficheiro './exemplo.py', linha 5
+    Erro sintático: alguma mensagem aqui
+        var cao : Animal func
     """
-    err_marker = "^"
-    message = str(error)
-    fmt_message = "\n".join([message, "-" * len(message)])
-    fmt_context = "\n".join(context)
-    # Doing this weird stuff to get the indicator '^' under the loc
-    err_line = context[len(context) - 1].split("|")
-    code_len = len(err_line[1].strip())
-    padding = len(err_line[0]) + 2
-    # find the size and create the error indicator
-    indicator = err_marker * code_len
-    indicator = indicator.rjust(padding + code_len)
-    return f"{fmt_message}\n{fmt_context}\n{indicator}\n"
+    filepath = path.relpath(error.fpath)
+    err_loc = f"linha {error.line}"
+    # Show column in case of syntax errors
+    if error.err_type == AmandaError.SYNTAX_ERR:
+        err_loc += f":coluna {error.col}"
+
+    err_header = f"""Ficheiro "{filepath}", {header}"""
+    err_msg = (
+        f"Erro sintático: {error.message}"
+        if error.err_type == AmandaError.SYNTAX_ERR
+        else "Erro: {error.message}"
+    )
+
+    return f"\n{err_header}\n{err_msg}\n    {context}\n"
 
 
-def throw_error(error, source):
-    """
-    Method that deals with errors thrown at different stages of the program.
+def throw_error(err: AmandaError):
+    # Attempt to get error line from file
+    filename = path.abspath(err.fpath)
+    assert path.isfile(filename), "Invalid filename supplied to error"
+    context = None
+    with open(filename, "r", encoding="utf8") as f:
+        for lineno, line in enumerate(f):
+            if lineno == err.line - 1:
+                context = line
+                break
+    # Line should always be valid because it came from file
+    assert context is not None, "Context should always be a line from the file"
 
-    param: source - io object where the program is being read from
-    Ex:
-
-    Erro sintático na linha 5: alguma mensagem aqui
-    ------------------------------------------------
-
-    3| var char : texto
-    4| var lobo : Animal
-    5| var cao : Animal func
-       ^^^^^^^^^^^^^^^^^^^^^
-    """
-    context = get_context(error, source)
     sys.stderr.write(fmt_error(context, error))
-    source.close()
     sys.exit()
 
 
