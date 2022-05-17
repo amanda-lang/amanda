3001fa2b7cd6f7e55276b09c4371a202d322f54e
stackswithans <stexor12@gmail.com>
2022-05-17 20:37:32 +0100

    New builtin file

---
+++ amanda/compiler/builtinfn.py
@@ -0,0 +1,16 @@
+import amanda.compiler.symbols as symbols
+from amanda.compiler.type import Kind, Type
+import enum
+import pathlib
+
+
+class BuiltinFn(enum.Enum):
+    VEC = "vec"
+    TAM = "tam"
+    ANEXA = "anexa"
+
+    def __str__(self) -> str:
+        return f"{self.value}"
+
+
+BUILTINS = {key.lower(): value for key, value in BuiltinFn.__members__.items()}
