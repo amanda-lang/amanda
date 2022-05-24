1dc67574968f8d85c09474aaf2c09397c78e44fa
stackswithans <stexor12@gmail.com>
2022-05-24 22:00:41 +0100

    Change crate to lib crate

--- amanda/vm/src/lib.rs
+++ amanda/vm/src/lib.rs
@@ -1,7 +1,33 @@
-pub mod alloc;
-pub mod ama_value;
-pub mod binload;
-pub mod builtins;
-pub mod errors;
-pub mod opcode;
-pub mod vm;
+use std::{env, fs, path::Path};
+
+mod alloc;
+mod ama_value;
+mod binload;
+mod builtins;
+mod errors;
+mod opcode;
+mod vm;
+
+use alloc::Alloc;
+use vm::AmaVM;
+
+fn main() -> Result<(), ()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Please specify a compiled bytecode file to run");
+        return Err(());
+    }
+
+    let file = Path::new(&args[1]);
+    let mut program_bin = fs::read(file).unwrap();
+    let mut alloc = Alloc::new();
+    let mut program = binload::load_bin(&mut program_bin, &mut alloc);
+    let mut vm = AmaVM::new(&mut program, alloc);
+    if let Err(err) = vm.run() {
+        eprint!("{}", err);
+        std::process::exit(1);
+    } else {
+        Ok(())
+    }
+}
