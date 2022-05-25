746c09473dbd92e804a23f03d5beed47b7a36054
stackswithans <stexor12@gmail.com>
2022-05-25 01:37:28 +0100

    Make int types clearer

--- amanda/libamanda.py
+++ amanda/libamanda.py
@@ -2,11 +2,13 @@ import ctypes
 from amanda.config import LIB_AMANDA_PATH
 
 
+# TODO: Use a struct to represent the module instead of using
+# BSON
 def run_module(module_bin: bytes) -> int:
     lib_ama = ctypes.CDLL(LIB_AMANDA_PATH)
     count = len(module_bin)
     # Note: Using ubyte to guarantee that arg 1 is an array of bytes
     # on all platforms
-    ByteArray = ctypes.c_ubyte * count
+    ByteArray = ctypes.c_uint8 * count
     bin_arr = ByteArray(*bytearray(module_bin))
-    return lib_ama.run_module(ctypes.byref(bin_arr), count)
+    return lib_ama.run_module(ctypes.byref(bin_arr), ctypes.c_int32(count))
